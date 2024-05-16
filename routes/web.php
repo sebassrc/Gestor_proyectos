8f6949c1fd8cfdef3244045c1a288ec590974130
Carlos fabian camilo caicedo <camilocaicedocarlosfabian40@gmail.com>
2024-05-15 23:21:54 -0500

    Web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProyectoController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,31 +20,32 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    // Rutas de perfil
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
-
-
-// Rutas proyectos
-Route::middleware('auth')->group(function () { 
+    // Rutas de proyectos
     Route::get('/proyectos', [ProyectoController::class, 'index'])->name('proyectos.index');
     Route::post('/proyectos', [ProyectoController::class, 'store'])->name('proyectos.store');
-    Route::get('/proyectos/new', [ProyectoController::class, 'create'])->name('proyectos.new');
     Route::get('/proyectos/create', [ProyectoController::class, 'create'])->name('proyectos.create');
     Route::delete('/proyectos/{proyecto}', [ProyectoController::class, 'destroy'])->name('proyectos.destroy');
     Route::put('/proyectos/{proyecto}', [ProyectoController::class, 'update'])->name('proyectos.update');
     Route::get('/proyectos/{proyecto}/edit', [ProyectoController::class, 'edit'])->name('proyectos.edit');
-    Route::get('/proyectos/{id}/download', 'ProyectoController@download')->name('proyectos.download');
-
-}); 
-
+    Route::get('/proyectos/{id}/download', [ProyectoController::class, 'download'])->name('proyectos.download');
+    
+    // Rutas de usuarios
+    Route::get('/users', [UserController::class, 'index'])->name('users.index');
+    Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
+    Route::post('/users', [UserController::class, 'store'])->name('users.store');
+    Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+    Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
+    Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+});
 
 require __DIR__.'/auth.php';
